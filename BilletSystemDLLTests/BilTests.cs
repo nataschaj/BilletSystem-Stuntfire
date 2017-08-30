1c56740f50ae6b921ccec32b35ed89700044b994
Søren Roed Schack <soer622t@edu.easj.dk>
2017-08-30 12:22:20 +0200

    Tilføjet en unittest der tester bilens pris.

---
+++ BilletSystemDLLTests/BilTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletSystemDLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletSystemDLL.Tests
+{
+    [TestClass()]
+    public class BilTests
+    {
+        [TestMethod()]
+        public void PrisTestBil()
+        {
+            //Arrange
+            var testBil = new Bil();
+
+            //Act
+            int pris = testBil.Pris();
+
+            //Assert
+            Assert.AreEqual(240, pris);
+        }
+    }
+}
