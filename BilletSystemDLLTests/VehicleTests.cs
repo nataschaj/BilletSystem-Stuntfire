24ff9c03634a496a1ec92780ce29d5c01d0c44c6
Søren Roed Schack <soer622t@edu.easj.dk>
2017-09-01 19:14:26 +0200

    Opgave 5b. Tilføjet en Vehicle test på TjekNummerplade-metoden, der tillader max 7 tegn i nummerpladen. Tjekker både 7 og 8 tegn, begge test er godkendt.

---
+++ BilletSystemDLLTests/VehicleTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletSystemDLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletSystemDLL.Tests
+{
+    [TestClass()]
+    public class VehicleTests
+    {
+        [TestMethod()]
+        public void TjekNummerpladeFor7Tegn()
+        {
+            //Arrange
+            var testVehicle = new Bil();
+
+            //Act
+            string nummerplade = testVehicle.TjekNummerplade(nummerplade: "AB34567");
+
+            //Assert
+            Assert.AreEqual("AB34567", nummerplade);
+        }
+
+        [TestMethod()]
+        public void TjekNummerpladeFor8Tegn()
+        {
+            //Arrange
+            var testVehicle = new Bil();
+
+            //Act
+            string nummerplade = testVehicle.TjekNummerplade(nummerplade: "AB345678");
+        }
+    }
+}
